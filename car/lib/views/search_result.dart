4ba8e7e3eeefff3afa18bc32fe1cf625295f2361
Glenn <100998394+EunhoKim98@users.noreply.github.com>
2025-01-20 17:17:12 +0900

    Update search_result.dart

--- car/lib/views/search_result.dart
+++ car/lib/views/search_result.dart
@@ -1,29 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:car/models/car_model.dart';
 
-class SearchResult extends StatefulWidget {
-  const SearchResult({super.key});
+class SearchResult extends StatelessWidget {
+  final CarModel carModel;
+  final String carNum;
 
-  @override
-  State<SearchResult> createState() => _SearchResultState();
-}
+  const SearchResult({Key? key, required this.carModel, required this.carNum}) : super(key: key);
 
-class _SearchResultState extends State<SearchResult> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(width: double.infinity,
-            child: Divider(color: Colors.grey, thickness: 2.0)),
-        Text(
-          "검색결과",
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
+    
+    // 스크롤 변경
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Divider(color: Colors.grey, thickness: 2.0),
           ),
-        ),
-        Container(width: double.infinity,
-            child: Divider(color: Colors.grey, thickness: 2.0)),
-      ],
+          SizedBox(height: 15),
+          Container(
+            width: double.infinity,
+
+            child: DataTable(
+                columns: [
+                  DataColumn(
+                    label: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '사고수준',
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '사고날짜',
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '차량번호',
+                      ),
+                    ),
+                  ),
+                ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(
+                        Container(
+                          child: Text(carModel.acdnKindNm),
+                        ),
+                      ),
+                      DataCell(
+                        Container(
+                          child: Text(carModel.acdnOccrDtm),
+                        ),
+                      ),
+                      DataCell(
+                        Container(
+                          child: Text(carModel.nowVhclNo),
+                        ),
+                      ),
+                    ]),
+                  ],
+                ),
+          ),
+
+          Container(padding: EdgeInsets.all(8)),
+          Icon(Icons.car_crash, color: Color.fromARGB(255, 0, 122, 255), size: 100),
+          Text("침 수 차 량", style: TextStyle(color: Color.fromARGB(255, 0, 122, 255), fontSize: 20)),
+        ],
+      ),
     );
   }
 }
